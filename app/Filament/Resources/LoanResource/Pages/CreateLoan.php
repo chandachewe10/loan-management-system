740e67939d4fddc7aefeb18ac044df65414e1f3c
chandachewe10 <chewec03@gmail.com>
2024-01-12 13:27:44 +0200

    Integrated with SWIFT-SMS

--- app/Filament/Resources/LoanResource/Pages/CreateLoan.php
+++ app/Filament/Resources/LoanResource/Pages/CreateLoan.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\LoanResource\Pages;
 
+use Illuminate\Support\Facades\Http;
+use App\Models\ThirdParty;
 use Carbon\Carbon;
 use App\Filament\Resources\LoanResource;
 use Bavix\Wallet\Models\Wallet;
@@ -47,13 +49,74 @@ class CreateLoan extends CreateRecord
             $data['loan_due_date'] = $loan_date->addYears($loan_duration);
         }
 
+
+        // Send an SMS to the Client depending on the ststus of the Loan Stage
+
+        $bulk_sms_config = ThirdParty::where('name', "=", 'SWIFT-SMS')->latest()->get()->first();
+        $borrower = \App\Models\Borrower::findOrFail($data['borrower_id'])->first();
+        $base_uri = $bulk_sms_config->base_uri ?? '';
+        $end_point = $bulk_sms_config->endpoint ?? '';
+        if (
+            $bulk_sms_config && $bulk_sms_config->is_active == 1 && isset($borrower->mobile)
+            && isset($base_uri) && isset($end_point) && isset($bulk_sms_config->token)
+            && isset($bulk_sms_config->sender_id)
+        ) {
+            // Define the JSON data
+            $url = $base_uri . $end_point;
+            $message = 'Hi ' . $borrower->first_name . ', ';
+
+            // Assuming $data['loan_status'] contains the current status
+            $loanStatus = $data['loan_status'];
+
+            switch ($loanStatus) {
+                case 'approved':
+                    $message .= 'Congratulations! Your loan application has been approved.';
+                    break;
+
+                case 'processing':
+                    $message .= 'Your loan application is currently under review. We will notify you once the review process is complete.';
+                    break;
+
+                case 'denied':
+                    $message .= 'We regret to inform you that your loan application has been rejected.';
+                    break;
+
+                case 'defaulted':
+                    $message .= 'Unfortunately, your loan is in default status. Please contact us as soon as possible to discuss the situation.';
+                    break;
+
+
+
+                default:
+                    $message .= 'Your loan application is in progress. Current status: ' . $loanStatus;
+                    break;
+            }
+
+
+            $jsonDataPayments = [
+                "sender_id" => $bulk_sms_config->sender_id,
+                "numbers" => $borrower->mobile,
+                "message" => 'Hi ' . $borrower->first_name . ' Your Loan application form has been processed and the current status is:' . $data['loan_status'],
+
+            ];
+
+            // Convert the data to JSON format
+            $jsonDataPayments = json_encode($jsonDataPayments);
+
+            Http::withHeaders([
+                'Authorization' => 'Bearer ' . $bulk_sms_config->token,
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ])
+                ->timeout(300)
+                ->withBody($jsonDataPayments, 'application/json')
+                ->get($url);
+        }
+
+
+
         // Check if the loan is being approved and they want to compile the Loan Agreement Form
         if ($data['loan_status'] === 'approved' && $data['activate_loan_agreement_form'] == 1) {
-
-
-
-
-
 
             //Check if they have the Loan Agreement Form template for this type of loan
             $loan_agreement_text = \App\Models\LoanAgreementForms::where('loan_type_id', "=", $data['loan_type_id'])->first();
@@ -73,8 +136,10 @@ class CreateLoan extends CreateRecord
                 $this->halt();
             } else {
 
+
+
                 // Remove the amount from the Specified Wallet
-                
+
                 $wallet->withdraw($data['principal_amount'], ['meta' => 'Loan amount disbursed from ' . $data['from_this_account']]);
 
 
