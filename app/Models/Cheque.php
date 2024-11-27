b112de1e94296c0a53f4c9e521800d185633ab8a
Alfiandri Putra Perdana <alfiandri.putra@gmail.com>
2024-11-27 22:50:31 +0700

    feat: add cheques model and table migration

---
+++ app/Models/Cheque.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cheque extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_11_27_152916_create_cheques_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cheques', function (Blueprint $table) {
+            $table->id();
+            $table->string('check_number');
+            $table->decimal('amount', 10, 2);            
+            $table->string('beneficiary');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cheques');
+    }
+};
