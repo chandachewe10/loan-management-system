d0e8b0316ec0b16098fd3339751334f30b9af416
chandachewe10 <chewec03@gmail.com>
2025-03-06 02:36:30 +0200

    Added Landing page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,392 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link
+      href="https://fonts.googleapis.com/css2?family=Hind:wght@600&family=Open+Sans:wght@400;600&display=swap"
+      rel="stylesheet"
+    />
+    <link href="./css/style.css" rel="stylesheet" />
+
+    <title>LENDIFY::HOME</title>
+  </head>
+  <body>
+    <header class="header">
+      <div class="container">
+        <div class="header__wrapper">
+          <a class="c-link" href="#">
+            <div class="c-logo">
+              <img src="img/logo.png" alt="Logo" class="c-logo__img" />
+              <span class="c-logo__text c-logo__text--white">Login</span>
+            </div>
+          </a>
+          <nav class="c-nav">
+            <input id="dropdown" class="c-nav__toggle" type="checkbox" />
+            <div class="c-nav__content">
+              <ul class="c-list c-list--flex">
+                <li class="c-list__item">
+                  <a href="#" class="c-link c-link--list">Pricing</a>
+                </li>
+                <li class="c-list__item">
+                  <a href="#" class="c-link c-link--list">Services</a>
+                </li>
+              </ul>
+              <button class="c-button c-button--small c-button--secondary">
+                Sign Up
+              </button>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <section class="section">
+        <div class="c-hero">
+          <div class="container">
+            <div class="c-hero__content">
+              <h1 class="heading heading--1 heading--light">
+                Grow Your Lending Business.
+                Ditch the Spreadsheets
+              </h1>
+              <h4 class="heading heading--4 heading--blue">
+                Lendfy is a loan management software that helps lenders automate workflows, reduce manual work, and launch new products with ease.
+              </h4>
+              <div class="c-hero__button-group">
+                <button class="c-button c-button--primary">
+                Login
+                </button>
+                <button class="c-button c-button--secondary">Sign Up</button>
+              </div>
+            </div>
+
+            <div class="c-hero__img-holder">
+              <img
+                class="c-hero__img"
+                src="img/dashboard.png"
+                alt="Dashboard"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="container">
+          <div class="section__title-wrapper">
+            <h2 class="heading heading--2">
+              Fully Automated
+            </h2>
+
+            <h4 class="heading heading--4">
+              Send personalized loan offers, agreements, and payment reminders via email or SMS. Predefined templates automate outreach, ensuring customers stay informed without adding to your workload.
+            </h4>
+          </div>
+
+          <div class="box box--flex">
+            <article class="c-card">
+              <div class="c-card__content">
+                <h3 class="c-card__title heading heading--3">
+                  Seamless collaboration
+                </h3>
+                <p class="c-paragraph c-card__text">
+                  Lendfy was built with ease of use in mind. Take a look at the user interface for LendFy and you will be sure to understand where the difference lies.
+                </p>
+              </div>
+              <div class="c-card__img-holder">
+                <img class="c-card__img" src="img/card-1.png" alt="" />
+              </div>
+            </article>
+
+            <article class="c-card">
+              <div class="c-card__content">
+                <h3 class="c-card__title heading heading--3">
+                  Live Updates
+                </h3>
+                <p class="c-paragraph c-card__text">
+                  Give customers instant reports to the status of their loan details, repayment schedules, and application statuses through <a href="https://swift-sms.net">Bulk SMS</a> Portal 
+                </p>
+              </div>
+
+              <div class="c-card__img-holder">
+                <img class="c-card__img" src="img/card-2.png" alt="" />
+              </div>
+            </article>
+
+            <article class="c-card">
+              <div class="c-card__content">
+                <h3 class="c-card__title heading heading--3">
+                 Flexibility
+                </h3>
+                <p class="c-paragraph c-card__text">
+                  Pre-configured loan agreement templates, settlements, and workflows are available to simplify loan management and ensure efficient operations.
+                </p>
+              </div>
+
+              <div class="c-card__img-holder">
+                <img class="c-card__img" src="img/card-3.png" alt="" />
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
+
+      <section class="section features">
+        <div class="container">
+          <div class="box box--grid">
+            <article class="c-feature">
+              <div class="c-feature__img-holder">
+                <img class="c-feature__img" src="img/feature-1.png" alt="" />
+              </div>
+              <div class="c-feature__content">
+                <h3 class="c-feature__title heading heading--3">
+                  Robust workflow
+                </h3>
+                <p class="c-paragraph c-feature__text">
+                  LendFy, with its built-in CRM also comes with built-in accounting service tools like expense management to better manage your business.
+                </p>
+              </div>
+            </article>
+
+            <article class="c-feature">
+              <div class="c-feature__img-holder">
+                <img class="c-feature__img" src="img/feature-2.png" alt="" />
+              </div>
+              <div class="c-feature__content">
+                <h3 class="c-feature__title heading heading--3">
+                Cloud Based
+                </h3>
+                <p class="c-paragraph c-feature__text">
+                  LendFy is a cloud-based hosted service. There is no need for you to have a website, servers or configure databases to start using our system.
+                </p>
+              </div>
+            </article>
+
+            <article class="c-feature">
+              <div class="c-feature__img-holder">
+                <img class="c-feature__img" src="img/feature-3.png" alt="" />
+              </div>
+              <div class="c-feature__content">
+                <h3 class="c-feature__title heading heading--3">
+                  Scalability
+                </h3>
+                <p class="c-paragraph c-feature__text">
+                 If your business has a website you can integrate it with LendFy to receive applications over API. Alternatively, if you don’t have a website we can always help you build one.
+                </p>
+              </div>
+            </article>
+          </div>
+
+          <div class="box box--flex box--mt2">
+            <button class="c-button c-button--primary">
+             Get Started
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="container">
+          <div class="section__title-wrapper">
+            <h2 class="heading heading--2">Simple pricing</h2>
+
+            <h4 class="heading heading--4">
+              LendFy is a SAAS platform and our pricing model is based on your growth. It starts from $20 per month and increases as your business grows.
+            </h4>
+          </div>
+
+          <div class="box box--grid box--gap2">
+            <article class="c-price">
+              <div class="c-price__header">
+                <div class="c-price__amount">
+                  <h3 class="c-price__amount--title heading heading--3">
+                    <span class="c-price__amount--secondary">$</span>
+                    <span class="c-price__amount--primary">20</span>
+                    <span class="c-price__amount--secondary">/m</span>
+                  </h3>
+                </div>
+                <p class="c-paragraph c-price__text">
+                You can cancel or change your plan at any time
+                </p>
+                <h5 class="heading heading--4 c-price__title">
+                  What's included
+                </h5>
+              </div>
+              <ul class="c-list">
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">Unlimited Users</div>
+                </li>
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">1000 Loans Max</div>
+                </li>
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">All other features Included</div>
+                </li>
+                
+              </ul>
+              <button
+                class="c-button c-button--secondary c-button--block c-price__button"
+              >
+                Get Started
+              </button>
+            </article>
+
+            <article class="c-price c-price--highlight">
+              <div class="c-price__header">
+                <div class="c-price__amount">
+                  <h3 class="c-price__amount--title heading heading--3">
+                    <span class="c-price__amount--secondary">$</span>
+                    <span class="c-price__amount--primary">54</span>
+                    <span class="c-price__amount--secondary">/m</span>
+                  </h3>
+                </div>
+                <p class="c-paragraph c-price__text">
+                  You can cancel or change your plan at any time.
+                </p>
+                <h5 class="heading heading--4 c-price__title">
+                  What's included
+                </h5>
+              </div>
+              <ul class="c-list">
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">Unlimited Users</div>
+                </li>
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">10,000 Loans Max</div>
+                </li>
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">All other features Included</div>
+                </li>
+               
+              </ul>
+              <button
+                class="c-button c-button--primary c-button--block c-price__button"
+              >
+          Get Started
+              </button>
+            </article>
+
+            <article class="c-price">
+              <div class="c-price__header">
+                <div class="c-price__amount">
+                  <h3 class="c-price__amount--title heading heading--3">
+                    <span class="c-price__amount--secondary">$</span>
+                    <span class="c-price__amount--primary">120</span>
+                    <span class="c-price__amount--secondary">/m</span>
+                  </h3>
+                </div>
+                <p class="c-paragraph c-price__text">
+                  You can cancel or change your plan at any time.
+                </p>
+                <h5 class="heading heading--4 c-price__title">
+                  What's included
+                </h5>
+              </div>
+              <ul class="c-list">
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">Unlimited Users</div>
+                </li>
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">Unlimited Loans</div>
+                </li>
+                <li class="c-list__item c-price__list-item">
+                  <div class="c-list__icon c-list__icon--check"></div>
+                  <div class="c-list__text">All other features Included</div>
+                </li>
+              
+              </ul>
+              <button
+                class="c-button c-button--secondary c-button--block c-price__button"
+              >
+               Get Started
+              </button>
+            </article>
+          </div>
+        </div>
+      </section>
+
+      <section class="section banner">
+        <div class="container">
+          <div class="c-banner">
+            <h2 class="heading heading--2 c-banner__title">
+              Request a Meeting
+            </h2>
+            <button class="c-button c-button--primary">Request now</button>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="c-footer">
+          <div class="c-footer__box">
+            <a class="c-link" href="#">
+              <div class="c-logo">
+                <img src="img/logo.png" alt="Logo" class="c-logo__img" />
+                <span class="c-logo__text c-logo__text--white">Login</span>
+              </div>
+            </a>
+            <p class="c-paragraph c-footer__text">
+              © 2025 Apply, all rights reserved
+            </p>
+          </div>
+          <div class="c-footer__box">
+            <div class="c-footer__icons">
+              <a href="#">
+                <img
+                  class="c-footer__icon"
+                  src="img/instagram.svg"
+                  alt="Instagram"
+              /></a>
+              <a href="#">
+                <img
+                  class="c-footer__icon"
+                  src="img/twitter.svg"
+                  alt="Twitter"
+                />
+              </a>
+              <a href="#">
+                <img
+                  class="c-footer__icon"
+                  src="img/facebook.svg"
+                  alt="Facebook"
+              /></a>
+            </div>
+            <ul class="c-list c-list--flex c-list--align-right">
+              <li class="c-list__item c-list__item--small">
+                <a href="#" class="c-link c-link--list c-link--list-right"
+                  >Contact</a
+                >
+              </li>
+              <li class="c-list__item c-list__item--small">
+                <a href="#" class="c-link c-link--list c-link--list-right"
+                  >About us</a
+                >
+              </li>
+              <li class="c-list__item c-list__item--small">
+                <a href="#" class="c-link c-link--list c-link--list-right"
+                  >FAQ</a
+                >
+              </li>
+              <li class="c-list__item c-list__item--small">
+                <a href="#" class="c-link c-link--list c-link--list-right"
+                  >Support</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
