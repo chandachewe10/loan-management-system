1b593caada05245c7cecab14335477380ab4a2f2
chandachewe10 <chewec03@gmail.com>
2025-03-14 09:33:47 +0200

    Add Landing page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
     <x-authentication-card>
+      
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,10 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
+<div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <br>
+    <div >
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>
