8c8cb7854578ee9ee92737730ead4c0fff5a897b
chandachewe10 <chewec03@gmail.com>
2025-04-14 23:52:43 +0200

    A wallet can be edited e.g funded by an investor

--- app/Filament/Resources/WalletResource.php
+++ app/Filament/Resources/WalletResource.php
@@ -86,7 +86,7 @@ class WalletResource extends Resource
             ])
             ->actions([
                
-                //Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
